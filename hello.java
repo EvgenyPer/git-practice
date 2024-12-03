6e900d41c6b9df7be7a341c7ee977ffb691a2c41
Evgeny Per <kunnka123.one123@gmail.com>
2024-12-04 00:42:31 +0300

    New text in the file from person2
    Added newfile.txt in dev branch

--- hello.java
+++ hello.java
@@ -4,5 +4,8 @@ public class HelloWorld {
         System.out.println("Язык программирования Java");
         System.out.println("Это простой пример кода.");
         System.out.println("Выводим текст на экран.");
+        System.out.println("Новый текст выводим на экран.");
+        System.out.println("Дополнительный текст выводим на экран.");
+
     }
 }
